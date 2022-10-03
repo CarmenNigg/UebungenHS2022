54637d5ae5e26b3e1b0dbafcde6aa8c2968136ef
CarmenNigg <carmen.nigg@students.fhnw.ch>
2022-10-03 20:39:18 +0200

    Uebung02

---
+++ Uebung02/Aufgabe01.js
@@ -0,0 +1,7 @@
+Array.from(Array(100).keys());
+
+var Array = [];
+for (Array % 2 !== 0; i < 99; i++){
+
+L.push(Array[i]);
+}
---
+++ Uebung02/Aufgabe02.js
@@ -0,0 +1,6 @@
+//Carmen Nigg
+
+var zufallszahl=Math.floor(6*Math.random())+1;
+function wuerfeln(){
+    console.log(parseFloat(zufallszahl)};
+
