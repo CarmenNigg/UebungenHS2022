9bc8e5d764fb4b3ec6e72cfc48453597b1663c55
CarmenNigg <carmen.nigg@students.fhnw.ch>
2022-10-24 20:34:33 +0200

    Uebung04_Design

--- Uebung04/projekt/src/Timer.js
+++ Uebung04/projekt/src/Timer.js
@@ -1,45 +1,64 @@
 import React, {Component} from "react";
+import Button from '@mui/material/Button';
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/Textfield";
+
 
 class Timer extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {count: this.props.countdown};
+        this.state = {count: this.props.countdown, text: "", render: true};
         this.interval = "0";
-
-        //Event-Handler:
-        this.update = this.update.bind(this);
+    
         this.start = this.start.bind(this);
+        this.update = this.update.bind(this);
+        this.time = this.time.bind(this);
     }
 
-    update() {
-        this.setState({ count: this.state.count - 1});
+    time(event) {
+        const sek = event.target.value;
+        this.setState({count: sek});
+    }
+
+    start() {
+        this.setState({count: this.state.count -1});
         if (this.state.count <= 1) {
-            this.setState({msg: "Fertig"});
-            this.setState({count: ""});
+            this.setState({count: "", text: "Fertig!!", render: true});
             clearInterval(this.interval);
             this.interval = "0";
         }
     }
 
-    start() {
-        this.setState({count: this.props.countdown})
+    update() {
+        this.setState({text: "", render: false});
 
-        if (this.interval != "0")
-        {
+        if (this.interval != "0") {
             clearInterval(this.interval);
         }
-        this.interval = setInterval(this.update, 1000)
+
+        this.interval = setInterval(this.start, 1000);
     }
+
 
     render() {
         return (<>
-            <hr/>
-            <h2>Timer 50 Sekunden</h2>
+            {this.state.render &&
+            <Grid>
+                <Grid>
+                    <Typography variant="body">Zeit einstellen:</Typography>
+                    <br/>
+                    <br/>
+                    <TextField label="Sekunden" value={this.state.countdown} onChange={this.time} variant='filled'></TextField>
+                </Grid>
+            </Grid>
+            }
+            <h4>verbleibende Zeit:</h4>
             <p>{this.state.count}</p>
-            <p>{this.state.msg}</p>
-            <button onClick={this.start}>Countcouwn starten</button>
-        </>);
+            <p>{this.state.text}</p>
+            <Button onClick={this.update} size="small" variant="contained" color="primary">Countdown starten</Button>
+        </>)
     }
 }
 
